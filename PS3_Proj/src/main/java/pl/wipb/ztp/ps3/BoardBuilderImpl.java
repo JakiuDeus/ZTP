bafb4aeea0bde38caa71b3e8c772f92221c8baa6
JakiuDeus <jakub.krypno@gmail.com>
2023-10-24 21:15:17 +0200

    PS3

---
+++ PS3_Proj/src/main/java/pl/wipb/ztp/ps3/BoardBuilderImpl.java
@@ -0,0 +1,27 @@
+package pl.wipb.ztp.ps3;
+
+public class BoardBuilderImpl implements BoardBuilderInterface {
+
+    public BoardBuilderImpl() {
+
+    }
+    @Override
+    public void addSegmentA() {
+
+    }
+
+    @Override
+    public void addSegmentB() {
+
+    }
+
+    @Override
+    public void addSegmentC() {
+
+    }
+
+    @Override
+    public void addSegmentG() {
+
+    }
+}
---
+++ PS3_Proj/src/main/java/pl/wipb/ztp/ps3/BoardBuilderInterface.java
@@ -0,0 +1,8 @@
+package pl.wipb.ztp.ps3;
+
+public interface BoardBuilderInterface {
+    void addSegmentA();
+    void addSegmentB();
+    void addSegmentC();
+    void addSegmentG();
+}
