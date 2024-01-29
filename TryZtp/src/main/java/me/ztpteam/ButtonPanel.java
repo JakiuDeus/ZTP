440e987ded697964b522e2893ea1aca4873df3ef
JakiuDeus <jakub.krypno@gmail.com>
2024-01-29 01:16:09 +0100

    yestes

--- TryZtp/src/main/java/me/ztpteam/ButtonPanel.java
+++ TryZtp/src/main/java/me/ztpteam/ButtonPanel.java
@@ -1,2 +1,38 @@
-package me.ztpteam;public class ButtonPanel {
+package me.ztpteam;
+
+import me.ztpteam.commands.Command;
+import me.ztpteam.commands.SeeInfoCommand;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+
+class ButtonPanel extends JPanel {
+    ImageIcon bulb = new ImageIcon("/lightbulb.jpg");
+
+    public ButtonPanel(MainFrame frame, Map<Integer, BasicComponent> components) {
+        setLayout(new GridLayout(4, 3));
+        components.keySet().forEach(k -> {
+            JButton button;
+            BasicComponent bc = components.get(k);
+            if (bc == null) {
+                button = new JButton("Niepodpięty");
+                button.setEnabled(false);
+                button.setBackground(Color.orange);
+            } else {
+                button = new JButton(String.valueOf(bc.getType()));
+                button.setIcon(bulb);
+                button.setHorizontalTextPosition(AbstractButton.CENTER);
+                button.setVerticalTextPosition(AbstractButton.BOTTOM);
+                button.setSize(175, 175);
+                List<Command> commandList = bc.getCommands();
+                commandList.addFirst(new SeeInfoCommand(bc, frame));
+                button.addActionListener(e -> new OptionFrame(commandList));
+            }
+            add(button);
+        });
+    }
+
+
 }
--- TryZtp/src/main/java/me/ztpteam/InfoPanel.java
+++ TryZtp/src/main/java/me/ztpteam/InfoPanel.java
@@ -1,2 +1,26 @@
-package me.ztpteam;public class InfoPanel {
+package me.ztpteam;
+
+import javax.swing.*;
+import java.awt.*;
+
+class InfoPanel extends JPanel {
+    private JLabel l1 = new JLabel("Empty");
+    private JLabel l2 = new JLabel("Empty2");
+    private Box box;
+
+    public InfoPanel() {
+        l1.setFont(new Font("Serif", Font.PLAIN, 26));
+        l2.setFont(new Font("Serif", Font.PLAIN, 26));
+        box = Box.createVerticalBox();
+        add(box);
+        box.add(l1);
+        box.add(l2);
+    }
+
+    public void setComponent(BasicComponent component) {
+        StringBuilder sb = new StringBuilder();
+        component.getType().forEach(type -> sb.append(type.getName()).append(" "));
+        l1.setText(sb.toString());
+        l2.setText("<html>" + component.getStatus() + "</html>");
+    }
 }
